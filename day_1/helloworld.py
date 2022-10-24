160459121baca1b89af0c96e96ebd849b2855506
Irenegarver <igarver3107@m.educaand.es>
2022-10-24 12:29:53 +0200

    Cambios realizados en eldia 1

---
+++ day_1/helloworld.py
@@ -0,0 +1,23 @@
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 % 4)
+print(3 // 4)
+
+"Irene"
+"García"
+"España"
+"I am enjoying 30 days of python"
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(["Asabeneh","Python","Finland"]))
+print(type("Irene"))
+print(type("García"))
+print(type("España"))
+
+
