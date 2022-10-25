964116b4cc4fae7a83d68b7c82b1caa04ebe6705
Irenegarver <igarver3107@m.educaand.es>
2022-10-25 13:39:36 +0200

    Cambios realizados en el dia 2

---
+++ day_2/variables.py
@@ -0,0 +1,19 @@
+#Dia 2: 30 dias de programación con python
+FirstName= 'Irene'
+LastName= 'García'
+FullName= 'Irene García'
+Country= 'España'
+City= 'Jerez'
+Age= '17'
+Year='2022'
+Is_Married= 'No'
+Is_True= 'Verdadera'
+Is_light_on= 'Encendida'
+Person_info= {
+    'FirstName': 'Irene',
+    'LastName': 'García',
+    'Country': 'España',
+    'City': 'Jerez'
+}
+
+print(type(FirstName))
