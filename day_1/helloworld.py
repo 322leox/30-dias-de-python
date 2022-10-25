cf9d1c57ec6b25323805a2de972d48fb55bb72e6
Irenegarver <igarver3107@m.educaand.es>
2022-10-25 13:14:06 +0200

    Cambios realizados en el dia 1

--- day_1/helloworld.py
+++ day_1/helloworld.py
@@ -21,3 +21,17 @@ print(type("García"))
 print(type("España"))
 
 
+# Dato integer:3,-5,2,-9
+# Dato float: 4,75, -3,27, -6,9, 7,43
+# Dato complex: 7+ j, 4 + 2j
+# Dato spring:  'María' , 'Australia', 'Ingles'
+# Dato booleans: True #Si la luz esta encendida, False#Si la luz esta apagada
+# Dato list: [0, 1, 2, 3, 4, 5,6 ,7]
+# Dato dictionaty: {'first_name'
+#                   'last_name'
+#                   'age'}
+# Dato truple: ('Mariposa', 'Caballo', 'Marte')
+# Dato set:{2, 14, 5, 22, -1}
+
+print(2-10)
+print(3-8)
