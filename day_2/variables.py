0890bc69cea9327d2966afe96e0c41dcc5629f33
Irenegarver <igarver3107@m.educaand.es>
2022-11-07 12:29:46 +0100

    Cambios realizados en el dia 2

--- day_2/variables.py
+++ day_2/variables.py
@@ -17,3 +17,24 @@ Person_info= {
 }
 
 print(type(FirstName))
+print(type(LastName))
+print(type(FullName))
+print(type(Country))
+print(type(City))
+print(type(Age))
+print(type(Year))
+print(type(Is_Married))
+print(type(Is_True))
+print(type(Is_light_on))
+print(type(Person_info))
+
+print(len(FirstName))
+print(len(LastName))
+
+num_one= '5'
+num_two= '4'
+num_theer= (num_one)+(num_two)
+print(num_theer)
+print(5+4)
+
+
