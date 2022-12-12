b68703793a7bcf3f534785944b1733d8707210ae
Irenegarver <igarver3107@m.educaand.es>
2022-12-12 12:39:37 +0100

    cambios en el dia 3

--- day_3/operators.py
+++ day_3/operators.py
@@ -7,8 +7,30 @@ base= 20
 Height= 10
 area= 0.5 * base * Height
 print(area)
-a=input(int('Cuanto mide a: '))
-b=input(int('Cuanto mide b: '))
-c=input(int('Cuanto mide c: '))
+a=input('Cuanto mide a: ')
+b=input('Cuanto mide b: ')
+c=input('Cuanto mide c: ')
 perimeter= a+b+c
 print(perimeter)
+
+l= input('El largo del triangulo: ')
+ach=input('El ancho del triangulo: ')
+Perimetro= 2* (l + ach)
+r=input('Cual es el radio: ')
+Acirculo= 3,14 * 3 ** 2
+circunferencia= 2 * 3,14 * r
+
+T= (2,2)
+G=(6,10)
+y2= 10
+y1=2
+x1=2
+x2= 6
+pendiente = (y2 - y1) / (x2- x1)
+euclidiana1= x1 - x2
+euclidiana2= y1 -y2
+print(pendiente)
+print(euclidiana1)
+print(euclidiana2)
+x=input('Cuanto vale x: ')
+Y= (x ** 2)+( 6 * x )+ 9
