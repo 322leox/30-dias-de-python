1342b8c95f81f0c97db396e0d333793d5830fa13
Irenegarver <igarver3107@m.educaand.es>
2022-12-12 11:58:21 +0100

    cambios del dia 3

---
+++ day_3/operators.py
@@ -0,0 +1,14 @@
+Age= 17
+height=float(160)
+number=complex(2,-4)
+print(number)
+
+base= 20
+Height= 10
+area= 0.5 * base * Height
+print(area)
+a=input(int('Cuanto mide a: '))
+b=input(int('Cuanto mide b: '))
+c=input(int('Cuanto mide c: '))
+perimeter= a+b+c
+print(perimeter)
