92c2829fe208ab6992075ab88cd4f392f8ef627c
Irenegarver <igarver3107@m.educaand.es>
2022-12-13 13:32:03 +0100

    cambios finales dia 3

--- day_3/operators.py
+++ day_3/operators.py
@@ -33,4 +33,38 @@ print(pendiente)
 print(euclidiana1)
 print(euclidiana2)
 x=input('Cuanto vale x: ')
-Y= (x ** 2)+( 6 * x )+ 9
+Y= (x * x)+( 6 * x )+ 9
+
+print(len('Python')  == len('dragon')) 
+
+print('on' in 'Python')
+print('on' in 'Dragon')
+print('jerga' in 'Espero que este curso no esté lleno de jerga')
+print('on'  not in 'Python')
+print('on' not in 'Dragon')
+
+print(float(len('Python')))
+p=input('Dime un numero: ')
+par= p % 2
+print(par is 0)
+d= 7 / 3 
+print(d is '2,7')
+print('10' is 10)
+print(int('9.8') is 10)
+hora= input('Cuantas horas trabaja: ')
+tarifa= input('cuanto gana a la hora: ')
+sueldo= hora * tarifa
+print(sueldo)
+
+N_Años=input('Dime una edad: ')
+one_day= 3600 * 24
+one_año= one_day * 365
+Edad= one_año * N_Años
+print(Edad)
+
+Tabla= (1 1 1 1 1
+        2 1 2 4 8
+        3 1 3 9 27
+        4 1 4 16 64
+        5 1 5 25 125)
+print(Tabla)
