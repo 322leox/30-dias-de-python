ffdee4cfb27b6828d582e8671bfed08a104b6cee
Irenegarver <igarver3107@m.educaand.es>
2023-01-31 13:42:15 +0100

    corregido

---
+++ day_8/Dictionaries.py
@@ -0,0 +1,11 @@
+perro= {}
+perro['Nombre'] = 'x'
+perro['Color'] = 'y'
+perro['Raza']= 'z'
+perro['Patas'] = 'l'
+perro['Edad'] = 'n'
+print(perro)
+Alumno ={'Nombre': 'name', 'Apellido': 'lastname', 'Sexo':'genero', 'Edad': 'años', 'Estado civil': 'Relación','Habilidad': ['interes'], 'Pais':'elmundo', 'Ciudad': 'city', 'direccion':'micasa'}
+print(len(Alumno))
+print(Alumno['Habilidad'])
+Alumno['Habilidad'] = ['Interes', 'Conocimiento', 'Arte', 'Imaginacion']
